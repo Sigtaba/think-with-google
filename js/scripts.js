082e4c9a70c2252b86d6558af71fd63161883567
Isabella Abatgis <abatgis@gmail.com>
2017-06-02 13:19:56 -0700

    add tools for marketers and sameheight jquery plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
   $(".button-collapse").sideNav();
+  $('.sameheight1').matchHeight();
+  $('.sameheight2').matchHeight();
+  $('.sameheight3').matchHeight();
+  $('.sameheight4').matchHeight();
 });
