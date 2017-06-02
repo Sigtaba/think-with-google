8ad81c7b2891139fb1c1229a7813434affd8c29d
Isabella Abatgis <abatgis@gmail.com>
2017-06-02 16:12:31 -0700

    add dropdowns to nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,25 @@ $(document).ready(function() {
   $('.sameheight2').matchHeight();
   $('.sameheight3').matchHeight();
   $('.sameheight4').matchHeight();
+  $('.drop1').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 18, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false // Stops event propagation
+  });
+  $('.drop2').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 18, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: true // Stops event propagation
+  }
+);
 });
